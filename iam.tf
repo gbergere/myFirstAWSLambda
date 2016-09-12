e270171c7fe90ba46dfb47f1085ecd8b1068b6e7
Geoffrey BERGERET <geoffrey.bergeret@onfido.com>
2016-09-12 17:17:21 +0100

    Add config file to define AWS region and IAM to allow lambda to log outputs.

---
+++ iam.tf
@@ -0,0 +1,41 @@
+
+resource "aws_iam_role" "r" {
+
+  name = "tf-my-first-lambda"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "logs" {
+
+  name = "tf-my-first-lambda-logs"
+  role = "${aws_iam_role.r.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
--- lambda.tf
+++ lambda.tf
@@ -1,35 +1,15 @@
 
-resource "aws_iam_role" "iam_for_lambda" {
+resource "aws_lambda_function" "my_first_lambda" {
 
-  name = "iam_for_lambda"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_lambda_function" "test_lambda" {
-
-  filename = "lambda_function_payload.zip"
+  filename = "lambda_payload.zip"
   runtime = "python2.7"
-  function_name = "lambda_function_name"
-  role = "${aws_iam_role.iam_for_lambda.arn}"
-  handler = "lambda_function_payload.lambda_handler"
-  source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  function_name = "tf-my-first-lambda"
+  role = "${aws_iam_role.r.arn}"
+  handler = "lambda_payload.lambda_handler"
+  source_code_hash = "${base64sha256(file("lambda_payload.zip"))}"
 
   provisioner "local-exec" {
-    command = "cd src && zip ../lambda_function_payload.zip *"
+    command = "cd src && zip ../lambda_payload.zip *"
   }
 
 }
